511c25501bb45c64c8fa96ccada581d503171b44
Constantino <inukisoft@gmail.com>
2017-07-13 00:41:14 -0400

    nuevos cambios con servicio de carga de países y bandera svg

--- comun/partial/home/home.js
+++ comun/partial/home/home.js
@@ -1,14 +1,14 @@
-angular.module('comun').controller('HomeCtrl',function($scope,agilservice,$log,$uibModal){
+angular.module('comun').controller('HomeCtrl',function($scope,agilservice,demorososervice,$log,$uibModal){
 
 	// servicio que viene en onpromise (porque es un retorno de un $http.get)
 	$scope.resultadoAgil = [];
+	$scope.bandera = null;
 
 	$scope.llamar = function() {
 
 		$scope.resultadoAgil = [];
 		var promise = agilservice.currentWeather();
 		
-
 		promise.then(
 			function (answer) {
 				$scope.resultadoAgil = answer; 
@@ -23,7 +23,7 @@ angular.module('comun').controller('HomeCtrl',function($scope,agilservice,$log,$
 		);
 	};
 
-	$scope.modal = function() {
+	$scope.abrirModal = function() {
 
         $scope.selectedPerson = "passed in person";
         console.log($scope.selectedPerson);
@@ -37,9 +37,52 @@ angular.module('comun').controller('HomeCtrl',function($scope,agilservice,$log,$
                 }
             }
         });
+	};
 
 
-	} 
+	// y el combito? data contiene tanto las opciones como el elemento
+	// que está seleccionado. El binding está en home.html, usando el 
+	// ng-model="data.model" y los valores se logran por medio de una iteración
+	$scope.data = {
+		model: null,
+		availableOptions: [
+			{value: null, name: 'cargando ... '}
+		]
+	};
+
+	demorososervice.listaPaisesEuropa().then(
+		function(data){
+			console.log("salida del servicio de países: " + JSON.stringify(data));
+			// el trabajo sucio consiste en tomar todos los países que vienen en el json
+			$scope.data.availableOptions = [];
+			for (var i=0; i < data.length; i++) {
+				$scope.data.availableOptions.push({name:data[i].name, value:data[i].alpha2Code});
+			}
+
+		},
+		function (response) {
+			console.log("el servicio ha dado ataos!");
+			// acá podemos mostrar algún texto y botón que permita la recarga
+			// de los países. 
+		}
+	);
+
+	$scope.actualizarBandera = function() {
+		// la idea es actualizar la bandera, obteniendo el código ISO alpha2
+		// almacenado por la selección del combo de países
+		if ($scope.data.model != null) {
+			// se invoca al servicio para traer el país según código
+			demorososervice.getPaisPorIsoAlpha2Code($scope.data.model).then(
+				function(data){
+					console.log("Recuperando país con iso " + $scope.data.model);
+					$scope.bandera = data.flag;
+				},
+				function(response){
+					console.log("ha ocurrido un error misterioso: " + JSON.stringify(response));
+				}
+			);
+		}
+	};
 
 
 });
--- comun/service/demorososervice.js
+++ comun/service/demorososervice.js
@@ -1,6 +1,32 @@
-angular.module('comun').factory('demorososervice',function() {
+angular.module('comun').factory('demorososervice',function($http,$q) {
 
     var demorososervice = {};
+
+    demorososervice.listaPaisesEuropa = function() {
+
+    	return $http.get("https://restcountries.eu/rest/v2/region/europe").then(
+    		function(response){
+    			return response.data;
+    		},
+    		function (response){
+    			return $q.reject(response);
+    		}
+    	);
+
+    };
+
+    demorososervice.getPaisPorIsoAlpha2Code = function(iso2) {
+
+        return $http.get("https://restcountries.eu/rest/v2/alpha/" + iso2).then(
+            function(response){
+                return response.data;
+            },
+            function (response){
+                return $q.reject(response);
+            }
+        );
+
+    };
 
     return demorososervice;
 });
